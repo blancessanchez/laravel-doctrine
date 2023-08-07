5c4e8b817b1cdcff2736c4c652e18a927f6e146f
blancessanchez <blancessanchez30@gmail.com>
2023-08-07 10:08:14 +0800

    add phpdocs

--- app/Http/Controllers/Api/DistrictController.php
+++ app/Http/Controllers/Api/DistrictController.php
@@ -7,12 +7,24 @@ use App\Repositories\DistrictRepository;
 use Illuminate\Http\Request;
 
 class DistrictController extends Controller
-{
+{    
+    /**
+     * __construct
+     *
+     * @param  DistrictRepository $districtRepository
+     * @return void
+     */
     public function __construct(protected DistrictRepository $districtRepository)
     {
         //
     }
 
+    /**
+     * store
+     *
+     * @param  \Illuminate\Http\Request $request
+     * @return void
+     */
     public function store(Request $request)
     {
         $district = $this->districtRepository->store($request);
@@ -20,13 +32,25 @@ class DistrictController extends Controller
         return response()->json($district, 201);
     }
 
+    /**
+     * getDistrictBasedOnStudentId
+     *
+     * @param  \Illuminate\Http\Request $request
+     * @return void
+     */
     public function getDistrictBasedOnStudentId(Request $request)
     {
         $student = $this->districtRepository->getDistrictBasedOnStudentId($request['id']);
 
         return response()->json($student, 200);
     }
-
+    
+    /**
+     * destroy
+     *
+     * @param  string $dataId
+     * @return void
+     */
     public function destroy($dataId)
     {
         $district = $this->districtRepository->destroy($dataId);
--- app/Types/MoneyType.php
+++ app/Types/MoneyType.php
@@ -9,13 +9,30 @@ use Money\Money;
 
 class MoneyType extends Type
 {
+    /**
+     * @var string
+     */
     const MONEY = 'Money';
 
+    /**
+     * getSQLDeclaration
+     *
+     * @param  array $fieldDeclaration
+     * @param  AbstractPlatform $platform
+     * @return string
+     */
     public function getSQLDeclaration(array $fieldDeclaration, AbstractPlatform $platform)
     {
         return 'VARCHAR(255)';
     }
 
+    /**
+     * convertToPHPValue
+     *
+     * @param  mixed $value
+     * @param  AbstractPlatform $platform
+     * @return Money|null
+     */
     public function convertToPHPValue($value, AbstractPlatform $platform)
     {
         if ($value === null) {
@@ -27,6 +44,13 @@ class MoneyType extends Type
         return new Money($decodedValue['amount'], new Currency($decodedValue['currency']));
     }
 
+    /**
+     * convertToDatabaseValue
+     *
+     * @param  mixed $value
+     * @param  AbstractPlatform $platform
+     * @return string
+     */
     public function convertToDatabaseValue($value, AbstractPlatform $platform)
     {
         if ($value === null) {
@@ -38,7 +62,12 @@ class MoneyType extends Type
             'currency' => $value->getCurrency()->getCode(),
         ]);
     }
-
+    
+    /**
+     * getName
+     *
+     * @return string
+     */
     public function getName()
     {
         return self::MONEY;
